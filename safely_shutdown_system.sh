7bd3d3ec313858c076c044b3fbecb90771a43956
JustScott <development@justscott.me>
2026-06-26 15:51:14 -0500

    Add a safe shutdown script
    Safely swapoff the swapfile, umount all the partitions, and luksClose the
    two encrypted partitions.

---
+++ safely_shutdown_system.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# safely_shutdown_system.sh - part of the DebianInstaller project
+# Copyright (C) 2026, Scott Wyman, development@scottwyman.me
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+PRETTY_OUTPUT_LIBRARY=./DebianInstaller/pretty_output_library.sh
+
+if ! source $PRETTY_OUTPUT_LIBRARY &>/dev/null
+then
+    printf "\n\n\e[31m%s %s\e[0m\n\n" \
+        "[!] Couldn't source the pretty output library. Make sure" \
+        "to run \`bash ./DebianInstaller/start_install.sh\`"
+    exit 1
+fi
+
+if swapon --show | grep "/swapfile" &>/dev/null
+then
+    swapoff /mnt/swapfile >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Turn of system swap"
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+
+if lsblk | grep "/mnt/boot/efi" &>/dev/null
+then
+    umount -R /mnt/boot/efi >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Umount /mnt/boot/efi"
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+if lsblk | grep "/mnt/boot" &>/dev/null
+then
+    umount -R /mnt/boot >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Umount /mnt/boot"
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+if lsblk | grep "/mnt/home" &>/dev/null
+then
+    umount -R /mnt/home >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Umount /mnt/home"
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+if lsblk | grep "/mnt" &>/dev/null
+then
+    umount -R /mnt >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Umount /mnt"
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+if lsblk | grep "crypt_root" &>/dev/null
+then
+    cryptsetup close crypt_root >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "close crypt_root"
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+if lsblk | grep "crypt_home" &>/dev/null
+then
+    cryptsetup close crypt_home >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "close crypt_home"
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+echo ""
+# Gives the user so many seconds to cancel the shutdown 
+for i in {5..1}; do
+    printf "\r%s \e[1;36m%s\e[0m" "Shutting Down In:" "$i"
+    sleep 1
+done
+
+shutdown now
